2e47cc9b703b90a5bf6e7af3c50d900c45280b55
wangqiang <wangqiang1996106@gmail.com>
2019-12-16 21:42:59 +0800

    init repo

---
+++ src/test/java/life/wangqiang/community/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.wangqiang.community.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
