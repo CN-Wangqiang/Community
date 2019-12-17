1547656437152cfe15bc33e8f6f2f8d5b1fd51bb
wangqiang <wangqiang1996106@gmail.com>
2019-12-17 22:59:51 +0800

    publish

---
+++ src/main/java/life/wangqiang/community/community/controller/PublishController.java
@@ -0,0 +1,74 @@
+package life.wangqiang.community.community.controller;
+
+import life.wangqiang.community.community.mapper.QuestionMapper;
+import life.wangqiang.community.community.mapper.UserMapper;
+import life.wangqiang.community.community.model.Question;
+import life.wangqiang.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @version : V1.0
+ * @ClassName: PublishController
+ * @Description: TODO
+ * @Auther: wangqiang
+ * @Date: 2019/12/17 下午6:18
+ */
+@Controller
+public class PublishController {
+    @Autowired
+    private  QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String toPublish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model){
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
+        User user =null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                user = userMapper.findByToken(token);
+                if (user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+        if (user == null){
+            model.addAttribute("error","用户未登录");
+            return "publish";
+        }
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+
+        questionMapper.create(question);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/life/wangqiang/community/community/mapper/QuestionMapper.java
@@ -0,0 +1,19 @@
+package life.wangqiang.community.community.mapper;
+
+import life.wangqiang.community.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @version : V1.0
+ * @ClassName: QuestionMapper
+ * @Description: TODO
+ * @Auther: wangqiang
+ * @Date: 2019/12/17 下午8:22
+ */
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,comment_count,view_count,like_count,tag) " +
+            "values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{commentCount},#{viewCount},#{likeCount},#{tag})")
+     void create(Question question);
+}
